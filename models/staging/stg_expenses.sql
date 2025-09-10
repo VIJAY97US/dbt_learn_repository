603e7a9e0e3be67b8ad3d2706c78ecb0cbe9909a
Vijayakumar Arumugam <softvijayakumar@gmail.com>
2025-09-10 02:30:32 +0000

    Commit

---
+++ models/staging/stg_expenses.sql
@@ -0,0 +1,33 @@
+
+{{ config(
+    materialized='table',
+    schema='staging'
+) }}
+
+WITH ranked_claims AS (
+    SELECT
+        claim_id,
+        employee_id,
+        claim_date,
+        expense_type,
+        claimed_amount,
+        currency,
+        approval_status,
+        approver_id,
+        ROW_NUMBER() OVER (PARTITION BY claim_id ORDER BY claim_date DESC) AS rn
+    FROM DBTPRACTICE.PUBLIC.EXPENSE_CLAIMS
+)
+
+SELECT
+    claim_id,
+    employee_id,
+    claim_date,
+    UPPER(expense_type) AS expense_type,
+    claimed_amount,
+    currency,
+    LOWER(approval_status) AS approval_status,
+    approver_id
+FROM ranked_claims
+WHERE rn = 1
+
+
