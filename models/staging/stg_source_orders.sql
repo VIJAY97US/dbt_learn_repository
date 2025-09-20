b30a6f6a4fdb9a6b31a7b8a2ab58b7243ccd2d50
Vijayakumar Arumugam <softvijayakumar@gmail.com>
2025-09-20 20:37:27 +0000

    v

--- models/staging/stg_source_orders.sql
+++ models/staging/stg_source_orders.sql
@@ -1,10 +1,2 @@
-SELECT
-    o.order_id,
-    o.customer_id,
-    o.order_date,
-    o.total_amount,
-    c.name AS customer_name,
-    c.email AS customer_email
-FROM {{ source('ecommerce', 'raw_orders') }} o
-JOIN {{ source('ecommerce', 'raw_customers') }} c
-  ON o.customer_id = c.customer_id
+select *
+from {{ source('my_source_name', 'raw_orders') }}
