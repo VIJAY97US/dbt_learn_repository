83b67a4654062899d9bc522a902e19f05920f603
Vijayakumar Arumugam <softvijayakumar@gmail.com>
2025-10-06 10:27:13 +0000

    Commit Changes

--- models/staging/Gen_Tst_orders.sql
+++ models/staging/Gen_Tst_orders.sql
@@ -3,5 +3,4 @@
     database='demo_db',
     schema='public'
 ) }}
-
 SELECT * FROM demo_db.public.Gen_Tst_orders;
--- tests/Tst_customers.sql
+++ tests/Tst_customers.sql
@@ -1,8 +1,7 @@
 {{ config(
-    database = 'DEMO_DB',
-    severity = 'error',
-    store_failure = true,
-    schema = 'public',
-    alias = 'invalid_Enddate'
+    materialized='table',
+    database='demo_db',
+    schema='public'
 ) }}
-SELECT * FROM demo_db.public.Gen_Tst_customers where CREATED_AT = '9999-12-31'
+
+SELECT * FROM demo_db.public.Gen_Tst_customers; 
